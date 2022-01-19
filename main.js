7e13b2d36e37e8167fa8b9ae6ac0e90f34bb88b5
AppuAzad <97887767+AppuAzad@users.noreply.github.com>
2022-01-19 15:57:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("My First Console Project");
+var name = "Ranbir";
+var last_name = "Kapoor";
+console.log(name.concat(last_name));
+
+var a = 5;
+var b = 10;
+
+var add=a + b;
+console.log(add);
+
+var subtract=a - b;
+console.log(subtract);
+
+var multiplication=a * b;
+console.log(multiplication);
+
+var d=a / b;
+console.log(d);
+
